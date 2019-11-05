4c3bae52a17d8277218cf55f8debdb18067416f5
Riley <weaselfat.riley@gmail.com>
2019-11-05 05:48:42 -0700

    testing my commit

--- bruteforce.cpp
+++ bruteforce.cpp
@@ -4,7 +4,6 @@
 #include<cmath>
 using namespace std;
 
-int N = 100;
 
 struct Point {
    double x;
@@ -18,39 +17,40 @@ double distance(Point a, Point b) {
 
 
 int main() {
-   vector<Point> p;
-   
-   string filename = "output.txt";
-   filename.insert(6, to_string(N));
+  int N = 100;
+  vector<Point> p;
 
-   ifstream file;
-   file.open(filename);
-   
-   for(int i=0; i<N; i++) {
-      double a, b;
-      file>>a>>b;
-      Point temp;
-      temp.x = a;
-      temp.y = b;
-      p.push_back(temp);
-   }
-   
-   double minDistance = 0.0;
-   int idxA;
-   int idxB;
-   for(int i=0; i<N; i++) {
-      for(int j=0; j<N; j++) {
-	 double currDistance = distance(p[i],p[j]);
-	 if(currDistance<minDistance){
-	    cout<<"3"<<endl;
-	    minDistance=currDistance;
-	    idxA = i;
-	    idxB = j;
-	 }
-      }
+  string filename = "output.txt";
+  filename.insert(6, to_string(N));
 
+  ifstream file;
+  file.open(filename);
+
+  for(int i=0; i<N; i++) {
+    double a, b;
+    file>>a>>b;
+    Point temp;
+    temp.x = a;
+    temp.y = b;
+    p.push_back(temp);
+  }
+
+  double minDistance = 0.0;
+  int idxA;
+  int idxB;
+  for(int i=0; i<N; i++) {
+    for(int j=0; j<N; j++) {
+      double currDistance = distance(p[i],p[j]);
+	    if(currDistance<minDistance){
+	       cout<<"3"<<endl;
+         minDistance=currDistance;
+	       idxA = i;
+	       idxB = j;
+       }
+     }
    }
-   
+
    cout<<"Minimum Distance is "<<minDistance<<" between points ";
    cout<<p[idxA].x<<","<<p[idxA].y<<" and "<<p[idxB].x<<","<<p[idxB].y<<endl;
+   
 }
